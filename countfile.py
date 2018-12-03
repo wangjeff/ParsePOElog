3419b50d478543c188e2eb1a411c73bb2aa2bd98
JeffWang <newgoalgo@gmail.com>
2018-12-03 18:55:39 +0800

    [fix]data overflow, will cause int to string error

--- countfile.py
+++ countfile.py
@@ -1,16 +1,21 @@
-# homework.py
 # -*- coding: utf-8 -*-
 import json
 import os
 
-ps_date = "1128_1\\"
+ps_date = "1203_7_in_out\\"
 ps_fix_path = "D:\\work\\POE\\POE_test_data\\POE_people\\"
 ps_file_type = ".txt"
-filter_badge1 = "ec21e54475ac"
-filter_badge2 = "ec21e59277ac"
+filter_badge1 = "ec21e54475ac"  # Bob
+filter_badge2 = "ec21e59776ac"  # rain_man
+filter_badge3 = "ec21e53875ac"  # happy
+filter_badge4 = "ec21e5ed6eac"  # sweet
+filter_badge5 = "ec21e5476fac"  # eilieen
+filter_badge6 = "ec21e5b066ac"  # alvin
+filter_badge7 = "ec21e5a070ac"  # small
+filter_badge8 = "ec21e5ce83ac"  # zhang
 filter_rssi = 89
-range_low = 1  # include equal
-range_upper = 11  # not include equal
+range_low = 22  # include equal
+range_upper = 24  # not include equal
 
 
 # read file data into string
@@ -31,7 +36,7 @@ def file_to_dict_data(file_name):
         i = i + 1
         mac_data = mac_data.lower().strip('\n').replace("\x00", "")
         sorted_data = mac_data.split(':')
-        if len(sorted_data) > 9:  # if equal to 1 ,represent the end of file & >5 means data have 6 values
+        if len(sorted_data) ==10:  # if equal to 1 ,represent the end of file & >5 means data have 6 values
             rssi_data = 256 + ~int(sorted_data[6], 16)
             # text_file.write("%s. MAC:%s RSSI:-%s Time:%s \n" % (i, SortedData[:6], Rssi_data, SortedData[7:]))
             if a != "":
@@ -60,7 +65,10 @@ def write_to_file(json_data_w, mac_file, mac_file_o1):
     for key in sort_key:
         # print("%s: %s" % (key, json_data_w[key]))
         if (json_data_w[key]['MAC'] != "d0d0d0d0d0d0") & (int(json_data_w[key]['RSSI']) < filter_rssi):
-            if (json_data_w[key]['MAC'] == filter_badge1) | (json_data_w[key]['MAC'] == filter_badge2):
+            if (json_data_w[key]['MAC'] == filter_badge1) | (json_data_w[key]['MAC'] == filter_badge2) \
+                    | (json_data_w[key]['MAC'] == filter_badge3) | (json_data_w[key]['MAC'] == filter_badge4) \
+                    | (json_data_w[key]['MAC'] == filter_badge5) | (json_data_w[key]['MAC'] == filter_badge6) \
+                    | (json_data_w[key]['MAC'] == filter_badge7) | (json_data_w[key]['MAC'] == filter_badge8):
                 print("%s , %s ,%s " % (json_data_w[key]['MAC'], json_data_w[key]['RSSI'], json_data_w[key]['TIME']))
                 text_file.write("\n%s , %s ,%s " % (json_data_w[key]['MAC'], json_data_w[key]['RSSI'], json_data_w[key]['TIME']))
 
@@ -85,16 +93,18 @@ def main():
 
     # if the file exist then delete the file
     for ikey in range(range_low, range_upper):
-        if os.path.exists(current_path + mac_file_o1 + str(ikey) + ps_file_type):
-            os.remove(current_path + mac_file_o1 + str(ikey) + ps_file_type)
+        temp_value = str(ikey)
+        if os.path.exists(current_path + mac_file_o1 + temp_value + ps_file_type):
+            os.remove(current_path + mac_file_o1 + temp_value + ps_file_type)
 
     for ikey in range(range_low, range_upper):
-        json_data1 = file_to_dict_data(current_path + mac_file_i1 + str(ikey) + ps_file_type)
-        json_data2 = file_to_dict_data(current_path + mac_file_i2 + str(ikey) + ps_file_type)
-        json_data3 = file_to_dict_data(current_path + mac_file_i3 + str(ikey) + ps_file_type)
-        write_to_file(json_data1, current_path + mac_file_i1 + str(ikey), current_path + mac_file_o1 + str(ikey) + ps_file_type)
-        write_to_file(json_data2, current_path + mac_file_i2 + str(ikey), current_path + mac_file_o1 + str(ikey) + ps_file_type)
-        write_to_file(json_data3, current_path + mac_file_i3 + str(ikey), current_path + mac_file_o1 + str(ikey) + ps_file_type)
+        temp_value = str(ikey)
+        json_data1 = file_to_dict_data(current_path + mac_file_i1 + temp_value + ps_file_type)
+        json_data2 = file_to_dict_data(current_path + mac_file_i2 + temp_value + ps_file_type)
+        json_data3 = file_to_dict_data(current_path + mac_file_i3 + temp_value + ps_file_type)
+        write_to_file(json_data1, current_path + mac_file_i1 + temp_value, current_path + mac_file_o1 + temp_value + ps_file_type)
+        write_to_file(json_data2, current_path + mac_file_i2 + temp_value, current_path + mac_file_o1 + temp_value + ps_file_type)
+        write_to_file(json_data3, current_path + mac_file_i3 + temp_value, current_path + mac_file_o1 + temp_value+ ps_file_type)
 
 
 if __name__ == '__main__':
